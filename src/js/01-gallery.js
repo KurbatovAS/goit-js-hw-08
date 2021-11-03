5cba61629afbe0f394d77b5d33a16db14e6f69f0
Andrey Kurbatov <nonamegod@gmail.com>
2021-11-03 23:37:47 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,19 @@
 // Add imports above this line
+import "simplelightbox/dist/simple-lightbox.min.js";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+let markup = createMarkup(galleryItems);
+galleryEl.innerHTML = markup;
+
+function createMarkup(galleryData) {
+  return galleryData.map(({preview, original, description}) => {
+      return `
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`
+  }).join('');  
+};
+
+const modal = new SimpleLightbox('.gallery a', { captions: true, captionsData: 'alt', captionDelay: 250 });
